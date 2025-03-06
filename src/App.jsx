c966b98cb862203c8f41c2076e75af53dd709e77
ghani kamil <ghanikamil19@gmail.com>
2025-03-06 09:47:03 +0700

    add halaman login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>tanggal 6</h1>
+      <Link to={"/login"}>halaman login</Link>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/assets/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router'
+
+function Login() {
+    let nav = useNavigate();
+    
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
